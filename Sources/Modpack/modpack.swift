0c8c26c5c19d0879c1f14e92802d2dfb059ae3f3
Samuel McBroom <sam@sammcb.com>
2022-09-10 19:26:41 -0700

    Bump version

--- Sources/Modpack/modpack.swift
+++ Sources/Modpack/modpack.swift
@@ -3,5 +3,5 @@ import ArgumentParser
 
 @main
 struct Modpack: AsyncParsableCommand {
-	static var configuration = CommandConfiguration(abstract: "A utility for managing modpacks.", version: "2.1.3", subcommands: [Update.self, Report.self])
+	static var configuration = CommandConfiguration(abstract: "A utility for managing modpacks.", version: "2.1.4", subcommands: [Update.self, Report.self])
 }
