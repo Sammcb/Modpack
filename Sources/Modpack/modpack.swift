76f271ac68ccfebb9a162fb169b5de2d846a7885
Samuel McBroom <sam@sammcb.com>
2023-06-27 09:38:07 -0700

    Add loader and version validation (#18)
    * Add loader and version validation (#15)
    
    * Fix build workflow

--- Sources/Modpack/modpack.swift
+++ Sources/Modpack/modpack.swift
@@ -3,5 +3,5 @@ import ArgumentParser
 
 @main
 struct Modpack: AsyncParsableCommand {
-	static var configuration = CommandConfiguration(abstract: "A utility for managing modpacks.", version: "4.1.0", subcommands: [Update.self, Report.self, Install.self])
+	static var configuration = CommandConfiguration(abstract: "A utility for managing modpacks.", version: "4.2.0", subcommands: [Update.self, Report.self, Install.self])
 }
--- Sources/Modpack/report.swift
+++ Sources/Modpack/report.swift
@@ -58,8 +58,9 @@ extension Modpack {
 		mutating func run() async throws {
 			logger.logLevel = .info
 			
-			let configData = try Data(contentsOf: ApiConfig.configFileURL)
-			let config = try ApiConfig.json5Decoder.decode(Config.self, from: configData)
+			try await validateConfig()
+			
+			let config = try ApiConfig.config
 			
 			let versionsString = "[\(versions.joined(separator: ", "))]"
 			
